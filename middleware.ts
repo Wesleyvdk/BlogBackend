e12ac8892ae7dae4fcbf9eb1efc11cd7105bd2d3
wesleyvdk <ehzgodd@vmi1792369.contaboserver.net>
2025-02-24 12:26:59 +0100

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
     const res = NextResponse.next();
 
+    // Handle preflight requests (OPTIONS method)
+    if (req.method === "OPTIONS") {
+        res.headers.set("Access-Control-Allow-Origin", "https://wesleyvanderkraan.vercel.app"); // Allow specific domain
+        res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.headers.set(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        return new NextResponse(null, { status: 204 });
+    }
+
+    // Set CORS headers for actual requests
     res.headers.set("Access-Control-Allow-Origin", "https://wesleyvanderkraan.vercel.app"); // Allow specific domain
     res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.headers.set(
@@ -10,10 +22,6 @@ export function middleware(req: NextRequest) {
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
 
-    // Handle preflight requests (OPTIONS method)
-    if (req.method === "OPTIONS") {
-        return new NextResponse(null, { status: 204 });
-    }
     return res;
 }
 
