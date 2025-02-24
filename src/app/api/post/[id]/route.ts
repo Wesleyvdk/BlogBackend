139c36c37a21d59f92bdd88e1796f3a47b3b8391
wesleyvdk <ehzgodd@vmi1792369.contaboserver.net>
2025-02-24 10:53:34 +0100

    added promise

--- src/app/api/post/[id]/route.ts
+++ src/app/api/post/[id]/route.ts
@@ -4,8 +4,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = context.params;
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const { id } = await context.params;
 
     try {
         const post = await prisma.post.delete({
@@ -18,8 +18,8 @@ export async function DELETE(req: NextRequest, context: { params: { id: string }
     }
 }
 
-export async function GET(req: NextRequest, context: { params: Record<string, string> }) {
-    const { id } = context.params;
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const { id } = await context.params;
 
     const post = await prisma.post.findUnique({
         where: { id: Number(id) },
